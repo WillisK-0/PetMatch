8932517b393c2f33b2727e6acfeb0ec0e9a9f754
Amy Roy <imachome@Amys-iMac.local>
2020-05-26 09:52:46 -0500

    establishing API token calls

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 // test comment - rickelle
-const express = require('express')
-const app = express()
-const mustacheExpress = require('mustache-express')
-const PORT = process.env.PORT || 8080
+const express = require("express");
+const app = express();
+const mustacheExpress = require("mustache-express");
+const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded());
 
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(PORT,()=>{
-    console.log('Server is running...')
-})
+app.listen(PORT, () => {
+  console.log("Server is running...");
+});
