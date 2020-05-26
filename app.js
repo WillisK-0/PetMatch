f277c6bf71bd7c42ffa2c1a97fc9442afc5c5f1d
Hannah Lima <nanaa_lima@hotmail.com>
2020-05-26 09:54:58 -0500

    nothing important

--- app.js
+++ app.js
@@ -1,27 +1,9 @@
 // test comment - rickelle
-const express = require('express')
-const app = express()
-const mustacheExpress = require('mustache-express')
-const PORT = process.env.PORT || 8080
+const express = require("express");
+const app = express();
+const mustacheExpress = require("mustache-express");
+const PORT = process.env.PORT || 8080;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(PORT,()=>{
-    console.log('Server is running...')
-})
+app.listen(PORT, () => {
+  console.log("Server is running...");
+});
